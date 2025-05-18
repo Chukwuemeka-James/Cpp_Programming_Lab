c19ed7db1cf4521db94393a12d01014963496ae7
Chukwuemeka-James <official.chukwuemekajames@gmail.com>
2025-05-18 18:24:26 +0100

    Data types in cpp

---
+++ 01_Basics/data_types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    int integerNum = 42;
+    float floatNum = 3.14f;
+    double doubleNum = 2.718281828;
+    char letter = 'C';
+    bool isStudent = true;
+    std::string text = "C++ is fun!";
+
+    std::cout << "The Integer is: " << integerNum << '\n';
+    std::cout << "The Float is: " << floatNum << '\n';
+    std::cout << "The Double is: " << doubleNum << '\n';
+    std::cout << "The Character is: " << letter << '\n';
+    std::cout << "The Boolean is: " << isStudent << '\n';
+    std::cout << "The String is: " << text << '\n';
+
+    return 0;
+}
