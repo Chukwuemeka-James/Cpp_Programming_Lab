accb1ba9723ed6eb5ac67fc0564a298749d5de0f
Chukwuemeka-James <official.chukwuemekajames@gmail.com>
2025-05-18 18:23:33 +0100

    Variable in cpp

---
+++ 01_Basics/variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    // Variable declaration and initialization
+    int age = 25;
+    double height = 5.9;
+    char grade = 'A';
+    std:: string name = "John";
+
+
+    // Displaying the values
+    std::cout << "Age: " << age << '\n';
+    std::cout << "Height: " << height << '\n';
+    std::cout << "Grade: " << grade << '\n';
+    std::cout << "Name: " << name << '\n';
+
+    return 0;
+}
